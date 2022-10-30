61fdaf3ce6b80fe4521e30719dc9fba05b2656d8
Md. Ashiquzzaman <ashiq.zaman@sigmasolutions.com.bd>
2022-10-30 16:52:40 +0600

    Add sail v1 init composer

--- sail-initializer.sh
+++ sail-initializer.sh
@@ -2,13 +2,12 @@
 
 set -e
 
-
 # Php minimum version check (minimum version should be lowest version laravel sail support)
 
 function version_gt() { test "$(printf '%s\n' "$@" | sort -V | head -n 1)" != "$1"; }
 
 CURRENT_PHP_VERSION=`php -v | head -n1 | cut -d " " -f 2`
-MIN_PHP_VERSION="7.3.0"
+MIN_PHP_VERSION=${MIN_PHP_VERSION:-"7.3.0"}
 
 if version_gt $MIN_PHP_VERSION $CURRENT_PHP_VERSION; then
     echo "Required php $MIN_PHP_VERSION, install or activate this php version before initialize!"
@@ -16,29 +15,40 @@ if version_gt $MIN_PHP_VERSION $CURRENT_PHP_VERSION; then
 fi
 
 # Check docker is running or not
-
 if ! docker info > /dev/null 2>&1; then
   echo "Docker is not running, please start docker first!"
   exit 1
 fi
 
+DOCKER_NETWORK_NAME=${DOCKER_NETWORK_NAME:-smartcomm}
+# Create docker network if not exists
+if ! docker network inspect $DOCKER_NETWORK_NAME > /dev/null 2>&1; then
+    docker network create --driver bridge $$DOCKER_NETWORK_NAME > /dev/null 2>&1
+fi
+
+
+# Check if project is valid composer project and support sail
+
+if [ ! -f composer.json ]; then
+    echo "Project is not valid composer project."
+    exit 1
+fi
+
+SAIL_VERSION=$(grep -o '"laravel/sail": "[^"]*' composer.json | grep -o '[^"]*$') > /dev/null || echo ""
+if [ -z "${SAIL_VERSION}" ]; then
+  echo "Laravel sail is not supported in this project."
+  exit 1;
+fi
+
 # Check sail exists, if not then init
+
 if ! [ -x "$(command -v vendor/bin/sail)" ]; then
   echo "Laravel sail not found, installing sail...."
-  COMPOSER=hacks/init/composer.json composer install --no-dev --working-dir=./
+  curl https://raw.githubusercontent.com/sigma-smartcomm/scripts/main/resources/composer/sail-1.json | sed "s/SAIL_VERSION/$SAIL_VERSION/g"  > /tmp/sail-1.json
+  COMPOSER=/tmp/sail-1.json composer install --no-dev --working-dir=./
   echo "Laravel sail installation done!"
+  rm -f /tmp/sail-1.json
 fi
-
-# Create docker network if not exists
-if ! docker network inspect smartcomm > /dev/null 2>&1; then
-    docker network create --driver bridge smartcomm > /dev/null 2>&1
-fi
-
-# Set proper permissions
-sudo chmod -R 0777 bootstrap
-sudo chmod -R 0777 storage
-sudo mkdir -p node_modules
-sudo chmod -R 0777 node_modules
 
 # Composer install for sail availability
 if [ -f composer.lock ]; then
@@ -50,11 +60,11 @@ if [ ! -f .env ]; then
     cp .env.example .env
 fi
 
-
 # Build
 vendor/bin/sail build
 vendor/bin/sail up -d
-vendor/bin/sail composer install
+vendor/bin/sail composer install --no-scripts
+
 vendor/bin/sail artisan key:generate
 vendor/bin/sail down
 
